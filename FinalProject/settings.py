ceafbad4c3dd61d301ed0f251ec486790acdc50f
Aviraj Arora <avirajarora1@gmail.com>
2022-01-11 01:44:54 +0530

    Update settings.py

--- FinalProject/settings.py
+++ FinalProject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$%#eu*l8ifa8dk7ht%q8vu1gr&c=0+5y-l@0cjx!^lm=v!y!66'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['aviraj-chat50.herokuapp.com','127.0.0.1']
 
 
 # Application definition

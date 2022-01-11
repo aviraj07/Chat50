3d0b3cf21153ec746429bdf2e4c20891e630a49a
Aviraj Arora <avirajarora1@gmail.com>
2022-01-11 16:12:17 +0530

    Update views.py

--- chat50/views.py
+++ chat50/views.py
@@ -73,13 +73,13 @@ def room(request, room):
     })
 
 def checkview(request):
-    room = request.POST['room_name']
+    room = request.POST['room_name'].strip()
     username = request.POST['username']
 
     if Room.objects.filter(name=room).exists():
         return redirect('/'+room+'/?username='+username)
     else:
-        new_room = Room.objects.create(name=room.strip())
+        new_room = Room.objects.create(name=room)
         new_room.save()
         return redirect('/'+room+'/?username='+username)
 
